d0a17d49a4aa4edb32cc45e59735d029e3b9c335
shanukashyap <60701526+shanukashyap@users.noreply.github.com>
2026-09-03 21:19:35 +0530

    Update 02_print_100_to_1.py

--- 02_print_100_to_1.py
+++ 02_print_100_to_1.py
@@ -1,7 +1,9 @@
+# Initialization: start number at 100
 number = 100
 
-# Initialization: start from 100
+# Condition: continue while number is greater than or equal to 1
 while number >= 1:
+
     print(number)
 
     # Update: decrease number by 1
