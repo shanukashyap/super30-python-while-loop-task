3a10808522a033ed4a6a754a439c819dd24c5ddb
shanukashyap <60701526+shanukashyap@users.noreply.github.com>
2026-09-03 21:20:43 +0530

    Update 07_factorial.py

--- 07_factorial.py
+++ 07_factorial.py
@@ -1,14 +1,15 @@
 number = int(input("Enter a number: "))
 
+# Initialization: start factorial at 1 and counter at 1
 factorial = 1
 i = 1
 
-# Initialization: i starts at 1
+# Condition: continue while counter is less than or equal to number
 while i <= number:
 
     factorial = factorial * i
 
-    # Update: increase i
+    # Update: increase counter by 1
     i = i + 1
 
 print("Factorial:", factorial)
