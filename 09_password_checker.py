bb338a448e402e1200d3f5afa7e0e1b07a46c792
shanukashyap <60701526+shanukashyap@users.noreply.github.com>
2026-09-03 21:21:07 +0530

    Update 09_password_checker.py

--- 09_password_checker.py
+++ 09_password_checker.py
@@ -1,12 +1,14 @@
 correct_password = "python123"
 
+# Initialization: get the first password attempt
 password = input("Enter password: ")
 
-# Continue asking while password is incorrect
+# Condition: continue while the password is incorrect
 while password != correct_password:
 
     print("Incorrect password. Try again.")
 
+    # Update: ask for another password attempt
     password = input("Enter password: ")
 
 print("Password correct. Access granted.")
